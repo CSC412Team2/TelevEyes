9478884151452298ac77f1125eae061b49c8ee37
Joshua Dixon <fpsbi00dsh0t@gmail.com>
2016-10-06 22:51:14 -0400

    Fade image transitions in RecyclerViewAdapter

--- app/src/main/java/edu/ecu/csc412/televeyes/adapter/ShowRecyclerViewAdapter.java
+++ app/src/main/java/edu/ecu/csc412/televeyes/adapter/ShowRecyclerViewAdapter.java
@@ -1,11 +1,15 @@
 package edu.ecu.csc412.televeyes.adapter;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.os.Build;
+import android.renderscript.Sampler;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.widget.TextView;
 
 import com.android.volley.toolbox.ImageLoader;
@@ -48,7 +52,6 @@ public class ShowRecyclerViewAdapter extends RecyclerView.Adapter<ShowRecyclerVi
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mItem = mValues.get(position);
-        //holder.mIdView.setText(mValues.get(position).name);
         holder.mTitleView.setText(mValues.get(position).show.name);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             holder.mSummaryView.setText(Html.fromHtml(mValues.get(position).show.summary, Html.FROM_HTML_MODE_COMPACT).toString());
@@ -62,7 +65,7 @@ public class ShowRecyclerViewAdapter extends RecyclerView.Adapter<ShowRecyclerVi
             String url = mValues.get(position).show.image.medium != null ? mValues.get(position).show.image.medium : mValues.get(position).show.image.original != null ? mValues.get(position).show.image.original : null;
 
             if (url != null) {
-                holder.mImageView.setImageUrl(url, mImageLoader);
+                fadeTransition(1, 0, holder.mImageView, url);
             }
         }
 
@@ -76,6 +79,41 @@ public class ShowRecyclerViewAdapter extends RecyclerView.Adapter<ShowRecyclerVi
                 }
             }
         });
+    }
+
+    public void fadeTransition(final float start, final float end, final View view, final String url){
+        ValueAnimator animator = ValueAnimator.ofFloat(start,end);
+        animator.setDuration(500);
+
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                view.setAlpha(animation.getAnimatedFraction());
+            }
+        });
+
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                    NetworkImageView networkImageView = (NetworkImageView) view;
+                    networkImageView.setImageUrl(url, mImageLoader);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        animator.start();
     }
 
     @Override
