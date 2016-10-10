582851ee0087809067e2bea8bf034891ad8495d4
Joshua Dixon <fpsbi00dsh0t@gmail.com>
2016-10-09 23:27:27 -0400

    First pass at search

---
+++ app/src/main/java/edu/ecu/csc412/televeyes/json/ShowContainer.java
@@ -0,0 +1,18 @@
+package edu.ecu.csc412.televeyes.json;
+
+/**
+ * Created by joshu on 10/9/2016.
+ */
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+
+public class ShowContainer {
+    @Expose
+    @SerializedName("score")
+    public double score;
+
+    @Expose
+    @SerializedName("show")
+    public Show show;
+}
